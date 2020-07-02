d2d01614153644b1b8f046ca5622b983fe37a1f7
BEN MILED Khalil <khalil.benmiled@soprahr.com>
2020-07-02 18:48:10 +0100

    shr-formation

--- SHR-E-Learning/src/main/java/com/soprahr/Utils/PROXY.java
+++ SHR-E-Learning/src/main/java/com/soprahr/Utils/PROXY.java
@@ -2,10 +2,10 @@ package com.soprahr.Utils;
 
 public class PROXY {
 
-	public static final String Utilisateurs="http://localhost:8762/SHR-Gestion-Utilisateurs";
-	public static final String SessionsFormations="http://localhost:8762/SHR-Gestion-SessionsFormations";
-	public static final String FormateursCabinets="http://localhost:8762/SHR-Gestion-FormateursCabinets";
-	public static final String Collaborateurs="http://localhost:8762/SHR-Gestion-Collaborateurs";
-	public static final String Besoins="http://localhost:8762/SHR-Gestion-Besoins";
-	public static final String ELearning="http://localhost:8762/SHR-E-Learning";
+	public static final String Utilisateurs="http://192.168.99.100:8762/SHR-Gestion-Utilisateurs";
+	public static final String SessionsFormations="http://192.168.99.100:8762/SHR-Gestion-SessionsFormations";
+	public static final String FormateursCabinets="http://192.168.99.100:8762/SHR-Gestion-FormateursCabinets";
+	public static final String Collaborateurs="http://192.168.99.100:8762/SHR-Gestion-Collaborateurs";
+	public static final String Besoins="http://192.168.99.100:8762/SHR-Gestion-Besoins";
+	public static final String ELearning="http://192.168.99.100:8762/SHR-E-Learning";
 }
